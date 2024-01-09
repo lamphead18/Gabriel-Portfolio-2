0ff56be7efd1ef4e3a999e749ce7c4aaee3c336d
gabrielfpigozzo@gmail.com <gabrielfpigozzo@gmail.com>
2024-01-09 16:57:34 -0300

    baixar curriculo funcionando e email adicionado

--- src/components/SocialMediaIcon.tsx
+++ src/components/SocialMediaIcon.tsx
@@ -2,6 +2,7 @@ import { Tooltip } from "@mui/material";
 import { FC, ReactNode } from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 interface SocialMediaIconProps {
   imgSrc: string;
@@ -9,23 +10,32 @@ interface SocialMediaIconProps {
 }
 
 const SocialMediaIcon: FC<SocialMediaIconProps> = ({ imgSrc, title }) => {
+  const handleEmailClick = () => {
+    const emailToCopy = "gabrielfpigozzo@gmail.com";
+    navigator.clipboard.writeText(emailToCopy);
+  };
+
   const mapImgSrcToIcon = (imgSrc: string): ReactNode => {
     switch (imgSrc) {
       case "linkedin":
         return <LinkedInIcon />;
       case "github":
         return <GitHubIcon />;
+      case "email":
+        return <EmailIcon onClick={handleEmailClick} style={{ cursor: 'pointer' }} />;
       default:
         return null;
-    };
+    }
   };
 
   const icon = mapImgSrcToIcon(imgSrc);
 
   return (
     <Tooltip title={title} placement="bottom" arrow>
-      <div className="flex items-center justify-center h-12 w-12 rounded-full bg-icons bg-icons cursor-pointer
-      hover:bg-secondary transition-all ease-linear duration-300">
+      <div
+        className="flex items-center justify-center h-12 w-12 rounded-full bg-icons bg-icons cursor-pointer
+      hover:bg-secondary transition-all ease-linear duration-300"
+      >
         {icon}
       </div>
     </Tooltip>
