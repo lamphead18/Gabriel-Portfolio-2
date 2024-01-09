8d65008bf693b2c8845bf1f8f8c3c88712da2b6f
Gabriel Pigozzo <gabriel@layoutsistemas.com.br>
2024-01-09 10:36:31 -0300

    First Commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  secondary?: boolean;
+  icon?: string;
+  onClick?: () => void;
+}
+
+const Button: FC<ButtonProps> = ({ children, secondary, icon, onClick }) => {
+  return <div>Button</div>;
+};
+
+export default Button;
